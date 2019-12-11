ace753063c244226ad6345497c90d745b3659de9
Nurakhmet <nura7kz@gmail.com>
2019-12-11 23:38:27 +0600

    Delete admin.py

--- Python/pharmacy/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Catalogue, Order, Account
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Catalogue)
-admin.site.register(Order)
-admin.site.register(Account)
-
